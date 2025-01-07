b9cab59b01021dbc4aee674247779f58cd850a3c
its-paras-gill <paras@impeccabletech.ca>
2025-01-07 09:46:27 +0530

    font size edited

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ async function fetchAndDisplayCSV(url, container) {
       row.forEach(cell => {
         const td = document.createElement('td');
         td.textContent = cell.trim();
-        td.style.fontSize = '1.2em';  // Add font size
+        td.style.fontSize = '1.0em';  // Add font size
         td.style.fontWeight = 'bold'; // Add bold
         tr.appendChild(td);
       });
